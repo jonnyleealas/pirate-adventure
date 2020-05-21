ec4e3ffa8ac0e425db6f8c8e7d6092023f46930a
jonathon lee <jonnyleealas@gmail.com>
2020-05-21 09:13:47 -0700

    side nav

--- js/app.js
+++ js/app.js
@@ -68,3 +68,17 @@ var loginForm = document.getElementById('form');
 if (loginForm !== null) {
   loginForm.addEventListener('submit', getUserName);
 }
+
+
+
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
